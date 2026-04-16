b8c392805a3a336d9522345567e0de582f462087
lukasdannemann <lukasdannemann@outlook.com>
2026-04-16 12:59:23 +0200

    edit hostname

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 
 export function getBaseUrl() {
   // if (window.location.hostname.includes("localhost")) {
-  return "http://localhost:3000/";
+  return "http://localhost:3000";
   // }
   
   // return "https://webbshop-2026-be-sigma.vercel.app";
